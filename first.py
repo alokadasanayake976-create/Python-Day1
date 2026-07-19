cb6a50a0b57506c1a955be9121d38ce18cc2c4c5
alokadasanayake976-create <alokadasanayake976@gmail.com>
2026-07-19 10:15:26 +0530

    Running Python Code

--- first.py
+++ first.py
@@ -1,4 +1,4 @@
-# #bilid in functions 
+# #------------------bilid in functions--------------- 
 # print("Hello orld!");
 # print("*"*10)
 # # /////////////////////////////////////////////////////
@@ -6,3 +6,17 @@
 #-------------------- Linting Code---------------------- 
 # print "Hello World"-----> Syntax error without parenthesis a red line appear 
 # 2 + ------ > in valid syntax 
+
+
+# -------------------Formatting code------------------- 
+# try to downloard a extention to code formater  -------> python PEPs 8 theory
+
+
+# -------------------Running Python Code---------------
+# we can put a keyward shortcut in commmand palette > open keybord sortcuts >run python file .
+# you can put any key ward ( command palet ---> ctrl+shift+p)
+
+
+
+
+
