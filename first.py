c2ef71a5a406b7feb968575392eb13f734d8486f
alokadasanayake976-create <alokadasanayake976@gmail.com>
2026-07-19 09:49:22 +0530

    linting code

--- first.py
+++ first.py
@@ -1,2 +1,8 @@
-#bilid in functions 
-print("Hello orld!");
+# #bilid in functions 
+# print("Hello orld!");
+# print("*"*10)
+# # /////////////////////////////////////////////////////
+
+#-------------------- Linting Code---------------------- 
+# print "Hello World"-----> Syntax error without parenthesis a red line appear 
+# 2 + ------ > in valid syntax 
